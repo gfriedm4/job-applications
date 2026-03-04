983c19bbdc92c711482d099446c10c9643564bc1
Gregory Friedman <greg@gregorys-mbp-2.mynetworksettings.com>
2026-03-04 13:53:35 -0500

    Switch e2e test import to chromatic playwright

--- tests/e2e/job-tracker.spec.ts
+++ tests/e2e/job-tracker.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/test";
+import { expect, test } from "@chromatic-com/playwright";
 
 test("create and inspect a job", async ({ page }) => {
   await page.goto("/#/");
